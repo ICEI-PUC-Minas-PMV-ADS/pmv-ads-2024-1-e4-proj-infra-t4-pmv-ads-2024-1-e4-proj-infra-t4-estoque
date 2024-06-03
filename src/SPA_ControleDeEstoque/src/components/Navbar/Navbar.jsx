332139c01a0f1a47f95de5a1c8ad37a327ce1fd8
ThiagoOliveiraQ <thiago.souza.1138412@sga.pucminas.br>
2024-06-02 21:10:39 -0300

    ajuste na logo

--- src/SPA_ControleDeEstoque/src/components/Navbar/Navbar.jsx
+++ src/SPA_ControleDeEstoque/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import logo1 from '../../assets/logo1.png'
 
 const Navbar = () => {
     return (
         <div className='nav'>
             <div className="nav-logo">
-                <img src="src\assets\logo1.png" alt="Controle de estoque" className='imagemLogo' />
+                <img src={logo1} alt="Controle de estoque" className='imagemLogo' />
             </div>
             <Link to="/login" className="nav-menu">
                 <li className="nav-saiba">Saiba Mais</li>
